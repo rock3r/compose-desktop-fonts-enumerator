be25154d037762a8f275d45f0eaf55056aa2a4ff
Sebastiano Poggi <poggos@gmail.com>
2021-12-22 19:13:36 +0100

    Use AWT to better match font families

--- src/main/kotlin/dev/sebastiano/composefontenumerator/SystemFonts.kt
+++ src/main/kotlin/dev/sebastiano/composefontenumerator/SystemFonts.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.text.platform.Font
 import com.sun.jna.platform.win32.Advapi32Util
 import com.sun.jna.platform.win32.WinReg
 import java.awt.GraphicsEnvironment
+import java.awt.font.TextAttribute
 import java.io.File
 import java.util.Locale
 import java.util.TreeMap
@@ -43,7 +44,9 @@ private const val FONTS_KEY_PATH = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVers
 private fun listFontFamiliesOnWindows(fontFamilyNames: Iterable<String>): List<SystemFontFamily> {
     val fontsDir = File("${System.getenv("WINDIR")}\\Fonts")
 
-    val registryMap = Advapi32Util.registryGetValues(WinReg.HKEY_LOCAL_MACHINE, FONTS_KEY_PATH) as TreeMap<String, String>
+    @Suppress("UNCHECKED_CAST")
+    val registryMap = (Advapi32Util.registryGetValues(WinReg.HKEY_LOCAL_MACHINE, FONTS_KEY_PATH) as TreeMap<String, String>)
+        .filterValues { it.endsWith(".ttf", ignoreCase = true) || it.endsWith(".otf", ignoreCase = true) }
 
     val fontPathByName = mutableMapOf<String, String>()
     for ((name, path) in registryMap.entries) {
@@ -61,23 +64,25 @@ private fun listFontFamiliesOnWindows(fontFamilyNames: Iterable<String>): List<S
 
     val fontFiles = fontPathByName.mapKeys { (key, _) -> key.substringBeforeLast(" (") }
         .mapValues { (_, path) -> if (path.contains('\\')) File(path) else File(fontsDir, path) }
-        .toMutableMap()
+        .mapKeys { (_, value) -> java.awt.Font.createFont(java.awt.Font.TRUETYPE_FONT, value) }
 
     return collectIntoSystemFontFamilies(fontFamilyNames, fontFiles)
 }
 
 private fun collectIntoSystemFontFamilies(
     fontFamilyNames: Iterable<String>,
-    fontFiles: MutableMap<String, File>
+    fontFiles: Map<java.awt.Font, File>
 ): MutableList<SystemFontFamily> {
     val sortedFontFamilyNames = fontFamilyNames.sortedByDescending { it.length }
     val fontFamilies = mutableListOf<SystemFontFamily>()
+    val filesByFont = fontFiles.toMutableMap()
+
     for (familyName in sortedFontFamilyNames) {
         println("Processing font family: $familyName")
-        val files = fontFiles.filterKeys { name -> name.startsWith(familyName) }
+        val files = filesByFont.filterKeys { font -> familyName.equals(font.family, ignoreCase = true) }
 
         for ((name, _) in files) {
-            fontFiles.remove(name)
+            filesByFont.remove(name)
         }
 
         if (files.isEmpty()) {
@@ -85,36 +90,58 @@ private fun collectIntoSystemFontFamilies(
             continue
         }
 
-        val fonts = files.map { (name, file) ->
-            val nameTokens = name.substring(familyName.length)
-                .split(' ', '-')
-                .map { it.trim().lowercase() }
-                .filter { it.isNotBlank() }
+        val fileFonts = files.map { (font, file) ->
+            val fontStyle = if (font.isItalic || looksItalic(font.name)) FontStyle.Italic else FontStyle.Normal
+            val rawWeight = fontWeightFromTextAttributeValue(font.attributes[TextAttribute.WEIGHT] as Float?)
+            val fontWeight = rawWeight ?: inferWeightFromName(
+                font.name.substringAfter(font.family).split(' ', '-')
+                    .map { it.trim().lowercase() }
+                    .filter { it.isNotBlank() }
+            )
 
-            Font(file = file, weight = inferWeightFromName(nameTokens), style = inferStyleFromName(nameTokens)) as FileFont
+            Font(file = file, weight = fontWeight, style = fontStyle) as FileFont
         }
 
-        fontFamilies += SystemFontFamily(familyName, FontFamily(fonts), fonts)
+        println("Font files:\n$fileFonts")
+
+        fontFamilies += SystemFontFamily(familyName, FontFamily(fileFonts), fileFonts)
     }
 
     return fontFamilies
 }
 
-private fun inferWeightFromName(nameTokens: List<String>): FontWeight =
-    when {
-        nameTokens.any { it == "thin" || it == "100" } -> FontWeight.Thin
-        nameTokens.any { it == "extralight" || it == "semilight" || it == "extra light" || it == "semi light" || it == "extra-light" || it == "semi-light" || it == "200" } -> FontWeight.ExtraLight
-        nameTokens.any { it == "light" || it == "300" } -> FontWeight.Light
-        nameTokens.any { it == "medium" || it == "500" } -> FontWeight.Medium
-        nameTokens.any { it == "semibold" || it == "semi bold" || it == "semi-bold" || it == "600" } -> FontWeight.SemiBold
-        nameTokens.any { it == "bold" || it == "700" } -> FontWeight.Bold
-        nameTokens.any { it == "extrabold" || it == "extra bold" || it == "extra-bold" || it == "800" } -> FontWeight.ExtraBold
-        nameTokens.any { it == "black" || it == "900" } -> FontWeight.Black
-        else -> FontWeight.Normal
+private fun looksItalic(name: String): Boolean = name.trimEnd().endsWith("italic", ignoreCase = true)
+
+// The mappings are somewhat arbitrary, and may look wrong, but this just going in order on both sides
+fun fontWeightFromTextAttributeValue(weightValue: Float?): FontWeight? =
+    when (weightValue) {
+        TextAttribute.WEIGHT_EXTRA_LIGHT -> FontWeight.Thin
+        TextAttribute.WEIGHT_LIGHT -> FontWeight.ExtraLight
+        TextAttribute.WEIGHT_DEMILIGHT -> FontWeight.Light
+        TextAttribute.WEIGHT_REGULAR -> FontWeight.Normal
+        TextAttribute.WEIGHT_SEMIBOLD -> FontWeight.Medium
+        TextAttribute.WEIGHT_MEDIUM -> FontWeight.SemiBold
+        TextAttribute.WEIGHT_BOLD -> FontWeight.Bold
+        TextAttribute.WEIGHT_HEAVY, TextAttribute.WEIGHT_EXTRABOLD -> FontWeight.ExtraBold
+        TextAttribute.WEIGHT_ULTRABOLD -> FontWeight.Black
+        else -> null
     }
 
-private fun inferStyleFromName(nameTokens: List<String>): FontStyle =
+private fun inferWeightFromName(nameTokens: List<String>): FontWeight =
     when {
-        nameTokens.contains("italic") -> FontStyle.Italic
-        else -> FontStyle.Normal
+        nameTokens.any { it.startsWith("thin") || it == "100" } -> FontWeight.Thin
+        nameTokens.any {
+            it.startsWith("extralight") || it.startsWith("semilight") || it.startsWith("extra light")
+                || it.startsWith("semi light") || it.startsWith("extra-light") || it.startsWith("semi-light") || it == "200"
+        } -> FontWeight.ExtraLight
+        nameTokens.any { it.startsWith("light") || it == "300" } -> FontWeight.Light
+        nameTokens.any { it.startsWith("medium") || it == "500" } -> FontWeight.Medium
+        nameTokens.any { it.startsWith("semibold") || it.startsWith("semi bold") || it.startsWith("semi-bold") || it == "600" } -> FontWeight.SemiBold
+        nameTokens.any { it.startsWith("bold") || it == "700" } -> FontWeight.Bold
+        nameTokens.any {
+            it.startsWith("extrabold") || it.startsWith("extra bold") || it.startsWith("extra-bold")
+                || it.startsWith("heavy") || it == "800"
+        } -> FontWeight.ExtraBold
+        nameTokens.any { it.startsWith("black") || it == "900" } -> FontWeight.Black
+        else -> FontWeight.Normal
     }
